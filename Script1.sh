0b6297676e4418533da7f500ca03f40d860f157a
Lenovo <tech.prashantg@gmail.com>
2025-05-20 19:11:30 +0530

    akashakash

--- Script1.sh
+++ Script1.sh
@@ -1,4 +1,5 @@
 echo "We are trying this practical from linux EC2"
 echo "I am from Young minds"
 echo "We are from batch-27 and learning DevOps"
-echo "my name is akash" 
+echo "my name is akash"
+echo "its akash "
